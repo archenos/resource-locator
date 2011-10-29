766ba98ac19031918e3ae05af2c20ee3248c9482
Kalpak Gadre <kalpakrg@gmail.com>
2011-10-29 16:29:41 +0530

    Added eclipse code formatting rules.

--- src/main/java/org/archenos/locator/classpath/CustomClassLoaderResourceLocator.java
+++ src/main/java/org/archenos/locator/classpath/CustomClassLoaderResourceLocator.java
@@ -3,7 +3,7 @@ package org.archenos.locator.classpath;
 public final class CustomClassLoaderResourceLocator extends AbstractClasspathResourceLocator {
 
     private final ClassLoader loader;
-    
+
     private CustomClassLoaderResourceLocator(ClassLoader loader) {
         super();
         this.loader = loader;
--- src/main/java/org/archenos/locator/classpath/SystemClassLoaderResourceLocator.java
+++ src/main/java/org/archenos/locator/classpath/SystemClassLoaderResourceLocator.java
@@ -1,6 +1,5 @@
 package org.archenos.locator.classpath;
 
-
 public final class SystemClassLoaderResourceLocator extends AbstractClasspathResourceLocator {
     public ClassLoader getClasssLoader() {
         return ClassLoader.getSystemClassLoader();
